856e7277eb240ed14ef0a0c4881247eeb06a3014
HaDucDev <haductrung392000@gmail.com>
2023-07-21 14:45:45 +0700

    update-postgree

--- src/main/java/com/haduc/beshop/BeshopApplication.java
+++ src/main/java/com/haduc/beshop/BeshopApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BeshopApplication {
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(BeshopApplication.class, args);
 	}
